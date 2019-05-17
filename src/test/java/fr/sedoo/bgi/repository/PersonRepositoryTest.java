c53972d95ef34b392f250941de5c56a08eb320d9
fandrelocal <fandrelocal@localhost>
2019-05-17 13:25:51 +0200

    Add mysql profile

--- src/test/java/fr/sedoo/bgi/repository/PersonRepositoryTest.java
+++ src/test/java/fr/sedoo/bgi/repository/PersonRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import fr.sedoo.bgi.domain.Person;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@ActiveProfiles("mysql")
 public class PersonRepositoryTest {
 
 	@Autowired
